0d7cc6930b14bfe08ade538500d038e34d5c11f4
Asemirski <al.semirski@gmail.com>
2017-06-23 15:32:55 +0300

    added init script and reconfigured files for it

--- script_aleksei.sh
+++ script_aleksei.sh
@@ -3,6 +3,8 @@ mkdir -p /opt/jenkins/bin
 mkdir /opt/jenkins/master
 cp /home/vagrant/jenkins.war /opt/jenkins/bin
 
+yum install java -y 
+
 useradd jenkins
 usermod -p $(echo root | openssl passwd -1 -stdin) jenkins
 usermod jenkins -G wheel
@@ -10,3 +12,7 @@ usermod jenkins -G wheel
 
 export JENKINS_HOME=/opt/jenkins/master
 
+cp /home/vagrant/jenkins.service /etc/systemd/system
+
+sudo su jenkins
+systemctl start jenkins.service
