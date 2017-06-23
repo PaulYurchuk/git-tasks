b026eb9a1c028de57b88a2fc61db7c3f4ec170b7
SuperBazis <bazi_s@mail.ru>
2017-06-23 17:50:36 +0300

    Nginx proxy-part added

--- nginxinst.sh
+++ nginxinst.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-yum -y install nginx 
+yum -y install nginx
+sed -i  's/ location \/ {/ location \/ {proxy_pass http:\/\/192.168.56.99:8080;/' /etc/nginx/nginx.conf;
+systemctl start nginx;
+systemctl enable nginx; 
