4e8194eaad05b720a58d9edff9a404a7907b23be
Asemirski <al.semirski@gmail.com>
2017-06-23 15:14:10 +0300

    folders test

--- script_aleksei.sh
+++ script_aleksei.sh
@@ -1,7 +1,12 @@
 #! /bin/bash
 mkdir -p /opt/jenkins/bin
-cp /home/vagrant/jenkins.war /optjenkins/bin
+mkdir /opt/jenkins/master
+cp /home/vagrant/jenkins.war /opt/jenkins/bin
 
 useradd jenkins
 usermod -p $(echo root | openssl passwd -1 -stdin) jenkins
 usermod jenkins -G wheel
+
+
+export JENKINS_HOME=/opt/jenkins/master
+
